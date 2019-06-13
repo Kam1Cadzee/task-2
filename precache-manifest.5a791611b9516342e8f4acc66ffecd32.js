a91732410d8ab3c7e9ef1a95f43fc109d4684a47
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-06-13 14:17:51 +0300

    Updates

---
+++ precache-manifest.5a791611b9516342e8f4acc66ffecd32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ae2387cd4d97d37a4f0dbcb7ca3485",
+    "url": "/task-2/index.html"
+  },
+  {
+    "revision": "11e973d7625beebbe038",
+    "url": "/task-2/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "aaa3d0df9bc5bf60ba6d",
+    "url": "/task-2/static/css/main.e1c9d510.chunk.css"
+  },
+  {
+    "revision": "11e973d7625beebbe038",
+    "url": "/task-2/static/js/2.f9ec9e08.chunk.js"
+  },
+  {
+    "revision": "aaa3d0df9bc5bf60ba6d",
+    "url": "/task-2/static/js/main.1f88a75f.chunk.js"
+  },
+  {
+    "revision": "27804743c9e6970b5f5c",
+    "url": "/task-2/static/js/runtime~main.d5f41ef3.js"
+  }
+]);
